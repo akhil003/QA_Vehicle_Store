b25af8a9b3d3ce510bb9aea59d9531014dcd45a2
akhil.shukla <akhil.shukla@S3LAP700>
2020-07-24 12:36:19 +0530

    Last commit

--- src/test/java/com/qa/test/Testing.java
+++ src/test/java/com/qa/test/Testing.java
@@ -12,11 +12,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-
 public class Testing {
 
 	WebDriver driver;
